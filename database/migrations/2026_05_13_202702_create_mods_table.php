5b73c8a5d0dcad078a8b3f62b97c9a128d1ecbc1
Turi53 <turi53@proton.me>
2026-05-13 22:22:35 +0100

    feat: add mods table migration

---
+++ database/migrations/2026_05_13_202702_create_mods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mods', function (Blueprint $table) {
+            $table->id();
+            $table->enum('mod_type', ['car', 'track']);
+            $table->text('description');
+            $table->string('download_link');
+            $table->boolean('is_premium')->default(false);
+            $table->unsignedInteger('download_count')->default(0);
+            $table->enum('link_status', ['active', 'broken', 'unchecked'])->default('unchecked');
+            $table->enum('status', ['published', 'unpublished', 'draft'])->default('draft');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->foreignId('author_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mods');
+    }
+};
