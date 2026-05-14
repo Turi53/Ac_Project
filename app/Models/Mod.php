5cd550f975063827e2e6c11639ba45412c67300f
Turi53 <turi53@proton.me>
2026-05-14 08:46:31 +0100

    feat: add MorphTo replationship to Mod

--- app/Models/Mod.php
+++ app/Models/Mod.php
@@ -4,17 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Mod extends Model
 {
     protected $fillable = [
-        'mod_type',
         'description',
         'download_link',
         'is_premium',
         'author_id',
         'published_at'
     ];
+
+    public function modable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 
     public function author(): BelongsTo
     {
