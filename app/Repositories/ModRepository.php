5035ff68b82abfb98c89dff9bf768d336876b639
Turi53 <turi53@proton.me>
2026-05-17 20:19:11 +0100

    feat: add getPublished getUnpublished getDrafts methods and tests to ModRepository

--- app/Repositories/ModRepository.php
+++ app/Repositories/ModRepository.php
@@ -2,7 +2,33 @@
 
 namespace App\Repositories;
 
+use App\Models\CarMod;
+use App\Models\Mod;
+use App\Models\TrackMod;
+
 class ModRepository
 {
+    public function getPublished(int $resultsPerPage = 9)
+    {
+        return Mod::whereIn('modable_type', [CarMod::class, TrackMod::class])
+            ->where('mods.status', 'published')
+            ->with(['modable'])
+            ->paginate($resultsPerPage);
+    }
 
+    public function getUnpublished(int $resultsPerPage = 9)
+    {
+        return Mod::whereIn('modable_type', [CarMod::class, TrackMod::class])
+            ->where('mods.status', 'unpublished')
+            ->with(['modable'])
+            ->paginate($resultsPerPage);
+    }
+
+    public function getDrafts(int $resultsPerPage = 9)
+    {
+        return Mod::whereIn('modable_type', [CarMod::class, TrackMod::class])
+            ->where('mods.status', 'draft')
+            ->with(['modable'])
+            ->paginate($resultsPerPage);
+    }
 }
--- tests/Feature/ModRepositoryTest.php
+++ tests/Feature/ModRepositoryTest.php
@@ -2,19 +2,66 @@
 
 namespace Tests\Feature;
 
+use App\Models\Mod;
+use App\Repositories\ModRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ModRepositoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private ModRepository $modRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->modRepository = new modRepository();
+    }
+
+    public function test_that_only_published_mods_are_retrieved(): void
+    {
+        Mod::factory()->count(4)->create([
+            'status' => 'draft'
+        ]);
+
+        Mod::factory()->count(8)->create([
+            'status' => 'published',
+        ]);
+
+        $mods = $this->modRepository->getPublished();
+
+        $this->assertSame(8, $mods->count());
+    }
+
+    public function test_that_only_unpublished_mods_are_retrieved(): void
+    {
+        Mod::factory()->count(4)->create([
+            'status' => 'published'
+        ]);
+
+        Mod::factory()->count(8)->create([
+            'status' => 'unpublished',
+        ]);
+
+        $mods = $this->modRepository->getUnpublished();
+
+        $this->assertSame(8, $mods->count());
+    }
+
+    public function test_that_only_draft_mods_are_retrieved(): void
+    {
+        Mod::factory()->count(4)->create([
+            'status' => 'published'
+        ]);
+
+        Mod::factory()->count(7)->create([
+            'status' => 'draft',
+        ]);
+
+        $mods = $this->modRepository->getDrafts();
+
+        $this->assertSame(7, $mods->count());
     }
 }
