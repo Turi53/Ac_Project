0ecf7c1943f6934ab9e334f3e9bb284419adc748
Turi53 <turi53@proton.me>
2026-05-17 15:30:00 +0100

    feat: add getPublished method and test to TrackModRepository

--- app/Repositories/TrackModRepository.php
+++ app/Repositories/TrackModRepository.php
@@ -2,7 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\TrackMod;
+
 class TrackModRepository
 {
-
+    public function getPublished(int $resultsPerPage = 9)
+    {
+        return TrackMod::join('mods', 'track_mods.id', '=', 'mods.modable_id')
+            ->where('modable_type', TrackMod::class)
+            ->where('mods.status', 'published')
+            ->select('track_mods.*')
+            ->with('mod.author')
+            ->paginate($resultsPerPage);
+    }
 }
--- tests/Feature/TrackModRepositoryTest.php
+++ tests/Feature/TrackModRepositoryTest.php
@@ -2,19 +2,41 @@
 
 namespace Tests\Feature;
 
+use App\Models\Mod;
+use App\Models\TrackMod;
+use App\Repositories\TrackModRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TrackModRepositoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private TrackModRepository $trackModRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->trackModRepository = new TrackModRepository();
+    }
+
+    public function test_that_only_published_track_mods_are_retrieved(): void
+    {
+        Mod::factory()->count(4)->create([
+            'modable_type' => TrackMod::class,
+            'status' => 'draft'
+        ]);
+
+        $trackMod = TrackMod::factory()->create();
+        Mod::factory()->create([
+            'status' => 'published',
+            'modable_id' => $trackMod->id,
+            'modable_type' => TrackMod::class,
+        ]);
+
+        $trackMods = $this->trackModRepository->getPublished();
+
+        $this->assertSame(1, $trackMods->count());
     }
 }
