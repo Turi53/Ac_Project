78d4798673b39c7fd7db0e1f13f1e59284e7b617
Turi53 <turi53@proton.me>
2026-06-07 11:18:55 +0100

    feat: add edit update and delete for car mods

--- app/Http/Controllers/Admin/CarModController.php
+++ app/Http/Controllers/Admin/CarModController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCarModRequest;
+use App\Http\Requests\UpdateCarModRequest;
 use App\Models\CarMod;
 use App\Services\AuthorService;
 use App\Services\MakeService;
@@ -77,16 +78,46 @@ class CarModController extends Controller
 
     public function edit(CarMod $carMod)
     {
-        //
+        return view('admin.car-mods.edit', [
+            'carMod' => $carMod,
+            'makes' => $this->makeService->getAllMakes(),
+            'authors' => $this->authorService->getAllAuthors()
+        ]);
     }
 
-    public function update(Request $request, string $id)
+    public function update(UpdateCarModRequest $request, CarMod $carMod)
     {
-        //
+        $carData = $request->safe()->only([
+            'model',
+            'year_of_manufacture',
+            'power',
+            'torque',
+            'zero_to_100',
+            'weight',
+            'top_speed',
+            'make_id'
+        ]);
+
+        $modData = $request->safe()->only([
+            'description',
+            'download_link',
+            'is_premium',
+            'author_id',
+        ]);
+        try {
+            $this->modService->updateCarMod($carMod->id, $carData, $modData);
+            return redirect()->route('admin.car-mods.index')
+                    ->with('success', 'Car mod Updated successfully');
+        } catch(Throwable $e) {
+            report($e);
+            return redirect()->back()->with('error', 'Something went wrong');
+        }
     }
 
     public function destroy(CarMod $carMod)
     {
-        //
+        $this->modService->deleteCarMod($carMod->id);
+
+        return redirect()->back()->with('success', 'The car has been successfully deleted');
     }
 }
--- resources/views/admin/car-mods/index.blade.php
+++ resources/views/admin/car-mods/index.blade.php
@@ -1,15 +1,35 @@
 <div>
-   <h2 class="">Logged in</h2>
-    <form action="{{ route('logout') }}" method="post">
-        @csrf
-        <input type="submit" value="logout">
-    </form>
-
     <h2>All Car Mods</h2>
     @foreach($carMods as $carMod)
         <div style="display:flex; background-color: antiquewhite; margin-bottom: 8px; align-items: center; gap: 4px; ">
             <h4>{{ $carMod->make->name }}</h4>
             <p>{{ $carMod->model }}</p>
+            <form action="{{ route('admin.car-mods.destroy', $carMod) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Delete" style="background-color: red">
+            </form>
+
+            <a href="{{ route('admin.car-mods.edit', $carMod) }}"
+               style="background-color: darkslategrey; color: white; padding: 4px;">
+                Edit
+            </a>
+
+            <form action="{{ route('admin.mods.publish', $carMod->mod) }}" method="POST">
+                @csrf
+                <input type="submit" value="Publish" style="background-color: limegreen; color: white;">
+            </form>
+
+            <form action="{{ route('admin.mods.unpublish', $carMod->mod) }}" method="POST">
+                @csrf
+                <input type="submit" value="Unpublish" style="background-color: orange; color: white;">
+            </form>
         </div>
     @endforeach
+
+    @if (session('success'))
+        <div style="background-color: palevioletred; color: white;">
+            {{ session('success') }}
+        </div>
+    @endif
 </div>
